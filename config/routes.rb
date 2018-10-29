dc11104e32e8738aff4c527bb7683ee34b05c95e
AshleyMatula <ashleysmatula@gmail.com>
2018-10-29 11:56:59 -0400

    4.3 Created new controllers, views and routes for orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :products
-  root 'simple_pages#index'
+  resources :orders, only: [:index, :show, :create, :destroy]
+  root 'simple_pages#landing_page'
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
